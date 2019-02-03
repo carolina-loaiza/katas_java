bf8dc954627b46c3ce68031d2ec4b4822da37efb
Carolina Loaiza <karolina801chan@gmail.com>
2019-02-02 18:18:00 -0600

    Added base project

---
+++ KataFooBarQix/src/com/cenfotec/katas/fbq/FooBarQix.java
@@ -0,0 +1,15 @@
+package com.cenfotec.katas.fbq;
+
+public class FooBarQix {
+
+	public static String process(int number) {
+		if (number % 3 == 0)
+			return "FOO";
+		if (number % 5 == 0)
+			return "BAR";
+		if (number % 7 == 0)
+			return "QIX";
+		return number+"";
+	}
+
+}
